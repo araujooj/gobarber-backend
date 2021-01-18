7978b1ed6ac4cd10f76ab94675ba5ad8e770d711
Gabriel Araujo <gabriel@kenzie.com.br>
2021-01-18 06:56:17 -0300

    feature: recover password service base logic started

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,49 @@
+import { injectable, inject } from 'tsyringe'
+import AppError from '@shared/errors/AppError';
+import { isAfter, addHours } from 'date-fns'
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokenRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+ token: string;
+ password: string;
+}
+
+@injectable()
+export default class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUsersRepository,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exist')
+    }
+
+    const user = await this.userRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exist')
+    }
+
+    const { created_at } = userToken
+
+    const compareDate = addHours(created_at, 2)
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired')
+    }
+
+    user.password = await this.hashProvider.generateHash(password);
+
+    await this.userRepository.save(user)
+  }
+}
